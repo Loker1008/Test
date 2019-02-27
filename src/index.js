00cac9b2baa777cdfd1d02b7f7723cbf279a46da
loker1008 <loker1008@yandex.ru>
2019-02-27 12:41:24 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import readlineSync from 'readline-sync';
 
 export function askQuestion(){
     console.log('Welcome to the Brain Games!');
+    console.log('');
 const actual = readlineSync.question('May I have your name?');
 
 console.log('Hello, ' + actual+'!');
