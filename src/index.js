1c976b4f14a056ad94f6f8552472defd0c711c9f
loker1008 <loker1008@yandex.ru>
2019-03-11 12:13:38 +0300

    qwe

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 
 export default function () {
   console.log('Welcome to the Brain Games!');
-  console.log('--------------------------------');
+  console.log('');
   const actual = readlineSync.question('May I have your name?');
   console.log(`Hello, ${actual}!`);
 }
