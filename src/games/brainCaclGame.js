bca6c92898cd4b1efed3c888d6a7acba37997d2e
loker1008 <loker1008@yandex.ru>
2019-03-14 11:07:26 +0300

    new function make Caculation

--- src/games/brainCaclGame.js
+++ src/games/brainCaclGame.js
@@ -1,36 +1,31 @@
 import readlineSync from 'readline-sync';
 import { createRndNumber, createRndOperator } from '../lib/rndFunctions';
 
+const makeCalculation = (operatorInt, firstNumber, secondNumber) => {
+  switch (operatorInt) {
+    case 0:
+      return { visualOperator: '+', result: firstNumber + secondNumber };
+    case 1:
+      return { visualOperator: '-', result: firstNumber - secondNumber };
+
+    default:
+      return { visualOperator: '*', result: firstNumber * secondNumber };
+  }
+};
+
 const startSecondGame = () => {
   const firstRndNumber = createRndNumber();
   const secondRndNumber = createRndNumber();
   const rndOperator = createRndOperator();
-  let visualOperator;
-  let result;
+  const calculate = makeCalculation(rndOperator, firstRndNumber, secondRndNumber);
 
-  switch (rndOperator) {
-    case 0:
-      visualOperator = '+';
-      result = firstRndNumber + secondRndNumber;
-      break;
-    case 1:
-      visualOperator = '-';
-      result = firstRndNumber - secondRndNumber;
-      break;
-
-    default:
-      visualOperator = '*';
-      result = firstRndNumber * secondRndNumber;
-      break;
-  }
-
-  const cliResponse = readlineSync.question(`Question: ${firstRndNumber} ${visualOperator} ${secondRndNumber}\n`);
-  const isRightResponse = (result === parseInt(cliResponse, 10));
+  const cliResponse = readlineSync.question(`Question: ${firstRndNumber} ${calculate.visualOperator} ${secondRndNumber}\n`);
+  const isRightResponse = (calculate.result === parseInt(cliResponse, 10));
 
   if (isRightResponse) {
     return { isRightAnswer: true, message: 'Correct!' };
   }
-  return { isRightAnswer: false, message: `'${cliResponse}' is wrong answer ;(. Correct answer was '${result}'.` };
+  return { isRightAnswer: false, message: `'${cliResponse}' is wrong answer ;(. Correct answer was '${calculate.result}'.` };
 };
 
 export default startSecondGame;
